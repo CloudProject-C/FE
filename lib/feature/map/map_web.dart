6981e9ad228f43b3641c745e20eed6aab880aa9d
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-12-16 23:55:13 +0900

    web

--- lib/feature/map/map_web.dart
+++ lib/feature/map/map_web.dart
@@ -1,28 +1,60 @@
 import 'dart:ui_web' as ui;
-import 'dart:html';
+import 'dart:html' as html;
+import 'dart:js_util' as js_util;
 import 'package:flutter/material.dart';
 
-class MapArea extends StatelessWidget {
+class MapArea extends StatefulWidget {
   final String? category;
 
-  MapArea({
+  const MapArea({
     super.key,
     required this.category,
-  }) {
-    ui.platformViewRegistry.registerViewFactory(
-      'naver-map',
-          (int viewId) {
-        final div = DivElement()
-          ..id = 'naver-map-div'
-          ..style.width = '100%'
-          ..style.height = '100%';
-        return div;
-      },
-    );
+  });
+
+  @override
+  State<MapArea> createState() => _MapAreaState();
+}
+
+class _MapAreaState extends State<MapArea> {
+  static bool _registered = false;
+  final String _viewType = 'naver-map-view';
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (!_registered) {
+      ui.platformViewRegistry.registerViewFactory(
+        _viewType,
+            (int viewId) {
+          final div = html.DivElement()
+            ..id = 'naver-map-$viewId'
+            ..style.width = '100%'
+            ..style.height = '100%';
+          return div;
+        },
+      );
+      _registered = true;
+    }
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      js_util.callMethod(
+        html.window,
+        'initNaverMap',
+        ['naver-map-0'],
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const HtmlElementView(viewType: 'naver-map');
+    return Container(
+      width: double.infinity,
+      height: 300, // 테스트용, 반드시 숫자
+      color: Colors.red, // 임시
+      child: const HtmlElementView(
+        viewType: 'naver-map-view',
+      ),
+    );
   }
 }
